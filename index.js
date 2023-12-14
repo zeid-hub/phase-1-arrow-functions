ef9be6ac93fab86838b123e107826449713dea6f
zeid <zeidhassan4@gmail.com>
2023-12-14 21:33:08 +0300

    Complited assignment

---
+++ index.js
@@ -0,0 +1,14 @@
+const divide = function(){
+    let sum = 2000/100
+    return sum
+}
+const square = () => {
+    let square = 2*2
+    return square
+}
+const add = () => {
+    let param1 = 3
+    let param2 = 4
+    let add = (param1 + param2)
+    return add
+}
